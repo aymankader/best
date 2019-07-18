e6ec3e47269c3e0783dba1cdeb31adc0d5fecbd1
aymankader <52969379+aymankader@users.noreply.github.com>
2019-07-19 01:44:55 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		for loop statement
+	</title>
+</head>
+<body>
+	<?php 
+
+	for($i=1;$i<20;$i++){
+		echo "the result is:$i+$i=".($i+$i)."<br>";
+
+
+	}
+
+	 ?>
+</body>
+</html>
